6f254f7fb2e77139afa24fafa9eedc29e798dc16
saraiin <saraennaciri96@gmail.com>
2023-03-17 09:27:29 -0700

    combo of 3 digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,31 +1,35 @@
 #include <stdio.h>
 /**
- * main - prints all possible different combinations of three digits
- * Return: Always 0 (Success)
+ * main - Entry point
+ *
+ * Description: This function will print the all combination of
+ * Three digits without repeating the combination by using putchar
+ *
+ * Return: 0 (Success)
  */
 int main(void)
 {
-        int n, m, l;
-        for (n = 48; n < 58; n++)
-        {
-                for (m = 49; m < 58; m++)
-                {
-                        for (l = 50; l < 58; l++)
-                        {
-                                if (l > m && m > n)
-                                {
-                                        putchar(n);
-                                        putchar(m);
-                                        putchar(l);
-                                        if (n != 55 || m != 56)
-                                        {
-                                                putchar(',');
-                                                putchar(' ');
-                                        }
-                                }
-                        }
-                }
-        }
-        putchar('\n');
-        return (0);
+	int first;
+	int second;
+	int third;
+
+	for (first = 0; first < 8; first++)
+	{
+		for (second = first + 1; second < 9; second++)
+		{
+			for (third = second + 1; third < 10; third++)
+			{
+				putchar(first + '0');
+				putchar(second + '0');
+				putchar(third + '0');
+				if (first < 7 || second < 8 || third < 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
