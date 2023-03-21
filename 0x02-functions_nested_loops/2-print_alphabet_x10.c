559fe0ed40c30bc7a388d0b71f4557d58ec3979d
saraiin <saraennaciri96@gmail.com>
2023-03-21 04:02:22 -0700

    islower

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,8 +6,12 @@
 void print_alphabet_x10(void)
 {
 	int i;
+	char j;
 
-	for (i = 0; i < 10 ; i++)
-		print_alphabet();
-	_putchar('\n');
+	for (i = 1 ; i <=  10 ; i++)
+	{
+		for (j = 'a' ; i <= 'z' ; j++)
+			_putchar(j);
+		_putchar('\n');
+	}
 }
