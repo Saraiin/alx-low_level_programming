7fc7910b70cee70a6a5fe7546f74fc924323e407
saraiin <saraennaciri96@gmail.com>
2023-03-27 05:47:30 -0700

    string reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - function that prints a string in reverse
+ * @s: string to reverse
+ */
+void print_rev(char *s)
+{
+	int l = 0;
+	int i;
+
+	while (*s != '\0')
+	{
+		l++;
+	}
+	for (i = l; i > 0; i--)
+	{
+		printf("%d", s[i]);
+	}
+	printf('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,4 +6,7 @@ int _strlen(char *s);
 void _puts(char *str);
 int _putchar(char c);
 void puts2(char *str);
+void puts_half(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
 #endif
