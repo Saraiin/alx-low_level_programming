73451f4ca90af13296870d3fb6e1d7065b857114
saraiin <saraennaciri96@gmail.com>
2023-04-03 15:51:31 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * _strstr - function thaat locates a substring
+ * @haystack: string
+ * @needle: substring
+ * Return: pointer to the beginning of the located substring
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0;
+	int k;
+	int j;
+
+	if (needle[0] == '\0')
+		return (haystack);
+	while (haystack[i] != '\0')
+	{
+		if (haystack[i] == needle[0])
+		{
+			k = i;
+			j = 0;
+			while (needle[j] != '\0')
+			{
+				if (haystack[k] == needle[j])
+					k++;
+					j++;
+				else
+					break;
+			}
+			if (needle[j] == '\0')
+			{
+				return (&haystack[i]);
+			}
+		}
+		i++;
+	}
+	return (NULL);
+}
