022c376b34a7dcc0ce4b8dc5f29db096b6d8465c
saraiin <saraennaciri96@gmail.com>
2023-04-03 07:25:33 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#define NULL 0
+/**
+ * _strchr - fuction that locates a charachter in a string
+ * @s: string
+ * @c: character
+ * Return: pointer of the first ocuurence of the character c
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+	int lenght = 0;
+
+	while (s[lenght] != '\0')
+		lenght++;
+
+	for (i = 0; i < lenght; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+		else
+		{
+			return (NULL);
+		}
+	}
+}
