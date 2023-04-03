28e51aa9bea5b63f5b3134fc9e0cf7738a946948
saraiin <saraennaciri96@gmail.com>
2023-04-03 14:21:13 -0700

    strspn

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,4 +3,4 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 void print_chessboard( char (*a)[8]);
 int _putchar(char c);
-
+unsigned int _strspn(char *s, char *accept);
