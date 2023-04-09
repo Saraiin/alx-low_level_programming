8e78c808c0629087c4e663e8f115de005ce59739
saraiin <saraennaciri96@gmail.com>
2023-04-09 16:31:06 -0700

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -5,14 +5,14 @@
  * main - adds positive numbers
  * @argc: number of argument passed
  * @argv: array of argument
- * Return: 1 if errors, 0 if success 
+ * Return: 1 if errors, 0 if success
  */
 int main(int argc, char *argv[])
 {
 	int s = 0;
 	int i, j;
 
-	for ( i = 1; i < argc; i++)
+	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j]; j++)
 		{
