ab0ffa60149d1490ae3fe0e62056213ae342debb
saraiin <saraennaciri96@gmail.com>
2023-04-13 12:00:53 -0700

    arraay of min and max

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * array_range - creates array of integers from min to max
+ * @min: minimum int in array
+ * @max: maximum int in array
+ *
+ * Return: pointer to array or null
+ */
+
+int *array_range(int min, int max)
+{
+	int *a;
+	int i, size;
+
+	if (min > max)
+		return (NULL);
+	size = (max - min) + 1;
+	a = malloc(size * sizeof(*a));
+	if (a == NULL)
+		return (NULL);
+	for (i = 0; i < size && min <= max; i++; min++)
+		a[i] = min;
+	return (a);
+}
