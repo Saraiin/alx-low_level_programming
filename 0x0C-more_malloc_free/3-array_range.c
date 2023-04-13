213ce6d355c664aed505667af7b4b10e8a3cdaeb
saraiin <saraennaciri96@gmail.com>
2023-04-13 12:58:13 -0700

    array min and max

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -16,7 +16,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 	size = (max - min) + 1;
-	a = malloc(size * sizeof(*a));
+	a = malloc(size * sizeof(int));
 	if (a == NULL)
 		return (NULL);
 	for (i = 0; i < size && min <= max; i++; min++)
