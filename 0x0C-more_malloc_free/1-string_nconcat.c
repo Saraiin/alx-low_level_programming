535b1538f8298d1ab7461327eb003dcec8627112
saraiin <saraennaciri96@gmail.com>
2023-04-13 13:36:02 -0700

    concatenate 2 string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -28,7 +28,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	for (i = 0; i < ln1; i++)
 		s[i] = s1[i];
-	for (j = 0; j < ln2 ; j++; i++)
+	for (j = 0; j < ln2 ; j++, i++)
 		s[i] = s2[j];
 	s[i] = '\0';
 	return (s);
