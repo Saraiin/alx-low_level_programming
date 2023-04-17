f94f127ed75ba27f28c9f62cf7dbba9e58dd7f5d
saraiin <saraennaciri96@gmail.com>
2023-04-17 11:20:59 -0700

    initialize variable struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,5 +13,13 @@ struct dog
 	float age;
 	char *owner;
 };
+/**
+ * dog_t - typedef for struct dog
+ */
+int _putchar(char);
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
 #endif
