e40e3304e2610f564a2efff3298644c7caf3bdd9
saraiin <saraennaciri96@gmail.com>
2023-04-19 05:18:52 -0700

    opcode

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,9 +10,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size > 0 && array != NULL && cmp && size)
+	if (size > 0 && array != NULL && cmp != NULL)
 		for (i = 0; i < size; i++)
+		{
 			if (cmp(array[i]))
 					return (i);
+		}
 	return (-1);
 }
