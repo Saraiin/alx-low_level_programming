5bccabffff35b9c097b366e6f16e8eb775e893b5
saraiin <saraennaciri96@gmail.com>
2023-05-02 11:20:01 -0700

    fix

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 /**
- * mem - allocate memorry for an array
+ * m - allocate memorry for an array
  * of pointers to nodes in linked list
  * @l: old list
  * @size: size of new list
  * @newnd: new node to add
  * Return: pointer to the new list
  */
-const listint_t **mem(const listint **l, size_t size, const listint_t *newnd)
+const listint_t **m(const listint_t **l, size_t size, const listint_t *newnd)
 {
 	const listint_t **newl;
 	size_t i = 0;
@@ -32,27 +32,27 @@ const listint_t **mem(const listint **l, size_t size, const listint_t *newnd)
 size_t free_listint_safe(listint_t **h)
 {
 	size_t i, n = 0;
-	listint_t **l = NULL;
+	const listint_t **l = NULL;
 	listint_t *nextnd;
 
-	if (head == NULL || *head == NULL)
+	if (h == NULL || *h == NULL)
 		return (n);
-	while (*head != NULL)
+	while (*h != NULL)
 	{
 		for (i = 0; i < n; i++)
 		{
-			if (*head == l[i])
+			if (*h == l[i])
 			{
-				*head = NULL;
+				*h = NULL;
 				free(l);
 				return (n);
 			}
 		}
 		n++;
-		l = mem(l, n, *head);
-		nextnd = (*head)->next;
-		free(*head);
-		*head = nextnd;
+		l = m(l, n, *h);
+		nextnd = (*h)->next;
+		free(*h);
+		*h = nextnd;
 	}
 	free(l);
 	return (n);
