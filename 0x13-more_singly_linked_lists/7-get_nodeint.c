01e354bc493c070f4dc23a2f6def12f8ec221b90
saraiin <saraennaciri96@gmail.com>
2023-05-02 09:52:06 -0700

    fix

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,12 +11,13 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *curnd;
 
 	curnd = head;
-	if (i == index)
-		return (curnd);
-	while (curnd && i < index)
+	while (curnd)
 	{
+		if (i == index)
+			return (curnd);
+	
 		i++;
 		curnd = curnd->next;
 	}
-	return (NULL);
+	return (curnd);
 }
