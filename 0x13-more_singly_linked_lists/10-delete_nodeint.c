4c98119407fc4ed410a7945400f408524e62c325
saraiin <saraennaciri96@gmail.com>
2023-05-02 10:32:52 -0700

    fix

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -16,7 +16,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	if (index == 0)
 	{
-		nxt = (*head)->nxt;
+		nxt = (*head)->next;
 		free(*head);
 		*head = nxt;
 		return (1);
