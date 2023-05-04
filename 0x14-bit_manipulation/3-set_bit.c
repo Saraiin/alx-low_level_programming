bec535f77b514f1eb9eda21800ba9f5892369ee3
saraiin <saraennaciri96@gmail.com>
2023-05-04 14:04:38 -0700

    set

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * set-bit sets value of a bit to 1
+ * set_bit - sets value of a bit to 1
  * @n: number
  * @index: index
  * Return: 1 if it worked, -1 if error
@@ -9,7 +9,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int value;
 
-	if (index > (sizeof((unsigned long int) * 8 - 1)))
+	if (index > 64)
 			return (-1);
 	value = 1 << index;
 	*n = *n | value;
