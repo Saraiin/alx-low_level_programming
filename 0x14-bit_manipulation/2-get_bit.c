b3ff1a53038282a2635087e4bffa27577fe3c540
saraiin <saraennaciri96@gmail.com>
2023-05-04 13:12:06 -0700

    2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,11 +2,12 @@
 /**
  * git_bit - return value of a bit a given index
  * @n: number to check
+ * @index, index to check
  * Return: value of the bit or -1 if error
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int a, c
+	unsigned long int a, c;
 	
 	if (index > (sizeof(unsigned long int) * 8 - 1 ))
 		return (-1);
