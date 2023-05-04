7ea31ec81fba8479f98ee147db437c704a1f2d7b
saraiin <saraennaciri96@gmail.com>
2023-05-04 16:02:43 -0700

    flip

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * get_endianness - checks the endianness
+ * Return: 1 if little, 0 if big
+ */
+int get_endianness(void)
+{
+	unsigned int n = 1;
+	char *c = (char*) &c;
+
+       return (int)*c;
+}       
