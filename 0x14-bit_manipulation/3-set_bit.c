ececfe77e1f8a647ce37cc343749f4e2321f632c
saraiin <saraennaciri96@gmail.com>
2023-05-04 14:06:38 -0700

    set

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,7 +10,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 	unsigned long int value;
 
 	if (index > 64)
-			return (-1);
+		return (-1);
 	value = 1 << index;
 	*n = *n | value;
 	return (1);
