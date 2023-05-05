e662c8d96ef624dd52e85d21db01ad17450b0158
saraiin <saraennaciri96@gmail.com>
2023-05-04 18:18:14 -0700

    fix 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,56 +1,28 @@
 #include "main.h"
 /**
- * numberbit - return number of bit
- * @s: string to check
- * Return: nb
- */
-int numberbit(const char *s)
-{
-	int nb = 0;
-
-	while (s != NULL)
-	{
-		nb++;
-		s++;
-	}
-	return (nb);
-}
-/**
- * poww - return a raised to the power of b
- * @a: base
- * @b: exponent
- * Return: result of a power b
- */
-int poww(int a, int b)
-{
-	unsigned int p = 1;
-
-	while (b)
-	{
-		p *= a;
-		b--;
-	}
-	return (p);
-}
-/**
- * binary_to_uint - convert a binary number to an unsigned int
- * @b: number
- * Return: converted number or 0
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: pointer to a string of 0 and 1 chars
+ * Return: the converted number, or 0 if there is invalid or b is NULL
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int result = 0, pow = 0, nbb = 0;
-	int i;
-
-	if (b == NULL)
+	unsigned int unitnum = 0;
+	
+	if (!b)
 		return (0);
-	nbb = numberbit(b);
-	for (i = nbb - 1; i >= 0; i--)
+	while (*b)
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (*b == '0' || *b == '1')
+		{
+			unitnum <<= 1;
+			if (*b == '1')
+				unitnum += 1;
+			b++;
+		}
+		else
+		{
 			return (0);
-		result += (b[i] - '0') * poww(2, pow);
-		pow++;
+		}
 	}
-	return (result);
+	return (unitnum);
 }
