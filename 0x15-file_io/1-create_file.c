705a5dfee6f7bbcfca8037eec8e43091111bb688
saraiin <saraennaciri96@gmail.com>
2023-05-08 15:11:45 -0700

    append

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,19 @@
 #include "main.h"
 /**
+ * _strlen - length of stringg
+ * @s: string
+ * Return: length of string
+ */
+size_t _strlen(char *s)
+{
+	size_t i;
+
+	for (i = 0; s[i]; i++)
+		;
+	return (i);
+}
+
+/**
  * create_file - create file and write content
  * @filename: name of file
  * @text_content: write content
@@ -7,18 +21,16 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int ptfile, i;
-	ssize_t ln = 0, charwrite = 0;
+	int ptfile;
+	ssize_t charwrite = 0;
 
 	if (filename == NULL)
 		return (-1);
 	ptfile = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (ptfile == -1)
 		return (-1);
-	for (i = 0; text_content[i]; i++)
-                ln++;
 	if (text_content != NULL)
-		charwrite = write(ptfile, text_content, ln);
+		charwrite = write(ptfile, text_content, _strlen(text_content));
 	close(ptfile);
 	if (charwrite == -1)
 		return (-1);
