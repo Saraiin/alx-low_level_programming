c86a63218576eab21e4b3d9cac1dbed3adde0c0a
saraiin <saraennaciri96@gmail.com>
2023-05-08 13:59:53 -0700

    creat and write

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,13 +7,13 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int ptfile;
+	int ptfile, i;
 	ssize_t ln = 0, charwrite = 0;
 
 	if (filename == NULL)
 		return (-1);
-	for (ln = 0; text_content[ln]; ln++)
-		;
+	for (i = 0; text_content[i]; i++)
+		ln++;
 	ptfile = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (ptfile == -1)
 		return (-1);
