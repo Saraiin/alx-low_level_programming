99962556c3a313140329e6457635f9a1ac7e55c0
saraiin <saraennaciri96@gmail.com>
2023-05-08 13:57:10 -0700

    creat and write

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,16 +4,17 @@
  * @filename: name of file
  * @text_content: write content
  * Retuen: -1 or 1
+ */
 int create_file(const char *filename, char *text_content)
 {
 	int ptfile;
-	size_t ln = 0, charwrite = 0;
+	ssize_t ln = 0, charwrite = 0;
 
 	if (filename == NULL)
 		return (-1);
 	for (ln = 0; text_content[ln]; ln++)
 		;
-	ptfile = open(filename, O_WRONGLY | O_CREAT | O_TRUNC, S_IRSUR | S_IWUSR);
+	ptfile = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (ptfile == -1)
 		return (-1);
 	if (text_content != NULL)
