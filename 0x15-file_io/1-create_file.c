a2b878df00bf60d596c9890a364d656cdf4e704e
saraiin <saraennaciri96@gmail.com>
2023-05-08 13:58:24 -0700

    creat and write

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,7 +3,7 @@
  * create_file - create file and write content
  * @filename: name of file
  * @text_content: write content
- * Retuen: -1 or 1
+ * Return: -1 or 1
  */
 int create_file(const char *filename, char *text_content)
 {
