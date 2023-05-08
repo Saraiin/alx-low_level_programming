41226a46146dba517cd93ef881f209e38cce67f3
saraiin <saraennaciri96@gmail.com>
2023-05-08 13:32:24 -0700

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * create_file - create file and write content
+ * @filename: name of file
+ * @text_content: write content
+ * Retuen: -1 or 1
+int create_file(const char *filename, char *text_content)
+{
+	int ptfile;
+	size_t ln = 0, charwrite = 0;
+
+	if (filename == NULL)
+		return (-1);
+	for (ln = 0; text_content[ln]; ln++)
+		;
+	ptfile = open(filename, O_WRONGLY | O_CREAT | O_TRUNC, S_IRSUR | S_IWUSR);
+	if (ptfile == -1)
+		return (-1);
+	if (text_content != NULL)
+		charwrite = write(ptfile, text_content, ln);
+	close(ptfile);
+	if (charwrite == -1)
+		return (-1);
+	return (1);
+}
