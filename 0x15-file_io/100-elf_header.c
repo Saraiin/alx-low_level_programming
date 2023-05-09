ee590557640b833c72e4bb9eaf23132488704815
saraiin <saraennaciri96@gmail.com>
2023-05-08 18:40:06 -0700

    advanced

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -159,4 +159,110 @@ void osabielf(const unsigned char *buffer)
 	else
 		printf("<unknown: %x>\n", buffer[EI_OSABI]);
 }
+/**
+ * abiverself - print ELF ABI version
+ * @buffer: the ELF header
+ */
+void abiverself(const unsigned char *buffer)
+{
+	printf("  %-34s %u\n", "ABI Version:", buffer[EI_ABIVERSION]);
+}
+/**
+ * typeelf - print ELF type
+ * @buffer: the ELF header
+ * @big_endian: endianness (big endian if non-zero)
+ */
+void typeelf(const unsigned char *buffer, int big_endian)
+{
+	char *type_table[5] = {
+		"NONE (No file type)",
+		"REL (Relocatable file)",
+		"EXEC (Executable file)",
+		"DYN (Shared object file)",
+		"CORE (Core file)"
+	};
+	unsigned int type;
 
+	printf("  %-34s ", "Type:");
+	if (big_endian)
+		type = 0x100 * buffer[16] + buffer[17];
+	else
+		type = 0x100 * buffer[17] + buffer[16];
+	if (type < 5)
+		printf("%s\n", type_table[type]);
+	else if (type >= ET_LOOS && type <= ET_HIOS)
+		printf("Processor Specific: (%4x)\n", type);
+	else
+		printf("<unknown: %x>\n", type);
+}
+/**
+ * entryelf - print entry point address
+ * @buffer: string containing the entry point address
+ * @bit_mode: bit mode (32 or 64)
+ * @big_endian: endianness (big endian if non-zero)
+ */
+void entryelf(const unsigned char *buffer, size_t bit_mode, int big_endian)
+{
+	int address_size = bit_mode / 8;
+
+	printf("  %-34s 0x", "Entry point address:");
+	if (big_endian)
+	{
+		while (address_size && !*(buffer))
+			--address_size, ++buffer;
+		printf("%x", *buffer & 0xff);
+		while (--address_size > 0)
+			printf("%02x", *(++buffer) & 0xff);
+	}
+	else
+	{
+		buffer += address_size;
+		while (address_size && !*(--buffer))
+			--address_size;
+		printf("%x", *buffer & 0xff);
+		while (--address_size > 0)
+			printf("%02x", *(--buffer) & 0xff);
+	}
+	printf("\n");
+}
+/**
+ * main - copy a file's contents to another file
+ * @argc: the argument count
+ * @argv: the argument values
+ *
+ * Return: Always 0
+ */
+int main(int argc, const char *argv[])
+{
+	unsigned char buffer[18];
+	unsigned int bit_mode;
+	int big_endian;
+	int fd;
+
+	if (argc != 2)
+	{
+		write(STDERR_FILENO, "Usage: elf_header elf_filename\n", 31);
+		exit(98);
+	}
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		write(STDERR_FILENO, "Error: Can't read from file\n", 28);
+		exir(98);
+	}
+	readf(fd, (char *) buffer, 18);
+	magicelf(buffer);
+	bit_mode = clasself(buffer);
+	big_endian = dataelf(buffer);
+	versionelf(buffer);
+	osabielf(buffer);
+	abiverself(buffer);
+	typeelf(buffer, big_endian);
+
+	lseek(fd, 24, SEEK_SET);
+	readf(fd, (char *) buffer, bit_mode / 8);
+	entryelf(buffer, bit_mode, big_endian);
+
+	closefile(fd);
+	return (0);
+}
