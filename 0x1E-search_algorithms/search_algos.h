a54dec3f6e2b87ec4f9996b49523b849dfcc7f7d
saraiin <saraennaciri96@gmail.com>
2023-10-11 14:28:55 -0700

    update header

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -24,4 +24,5 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
 #endif /* SEARCH_ALGOS_H */
